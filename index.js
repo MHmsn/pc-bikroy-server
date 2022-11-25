a7fa48b46df08cce0c1c6f4d6ecf1b3c87b90a3e
Md. Moinul Hassan <moinulh.msn@gmail.com>
2022-11-25 22:52:17 +0600

    added jwt and api

--- index.js
+++ index.js
@@ -13,9 +13,57 @@ require('dotenv').config();
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.nhrod4k.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send('unauthorized access');
+    }
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+        if(err){
+            return res.status(403).send({message: 'Forbidden Access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run(){
     try{
-
+        const categoryCollection = client.db('PCBikroy').collection('Categories');
+        const usersCollection = client.db('PCBikroy').collection('Users');
+        
+        app.get('/categories', async(req, res) => {
+            const query={ };
+            const categories = await categoryCollection.find(query).toArray();
+            res.send(categories);
+        })
+        app.get('/user', verifyJWT, async(req, res) => {
+            const email = req.query.email;
+            const query = {email: email};
+            const user = await usersCollection.findOne(query);
+            
+            res.send(user);
+        })
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = {email: email};
+            const user = await usersCollection.findOne(query);
+            if(user){
+                const token = jwt.sign({email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'});
+                return res.send({accessToken: token});
+            }
+            console.log(user);
+            res.status(403).send({accessToken: ''});
+        })
+        app.post('/users', async(req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
     }
     finally{
         
